0fc3c5fea5d494a81141f62187618a1b5e2cca48
Eyosias Desta <desta.eyosias@gmail.com>
2022-01-13 12:27:59 +0300

    restaurant component added

--- screens/Home.js
+++ screens/Home.js
@@ -1,17 +1,28 @@
 import React from "react";
-import { View, Text, SafeAreaView, StatusBar } from "react-native";
+import { View, Text, SafeAreaView, StatusBar, ScrollView } from "react-native";
 import HeaderApps from "../components/HeaderTabs.js";
 import SearchBar from "../components/SearchBar.js";
 import Categories from "../components/Categories.js";
+import RestaurantItem from "../components/RestaurantItem.js";
 
 export default function Home() {
   return (
-    <SafeAreaView style={{backgroundColor: "#eee", flex: 1}}>
-      <View style={{backgroundColor: "white", paddingTop: 15, paddingBottom: 60, marginTop: StatusBar.currentHeight}}>
-      <HeaderApps />
-      <SearchBar />
+    <SafeAreaView style={{ backgroundColor: "#eee", flex: 1 }}>
+      <View
+        style={{
+          backgroundColor: "white",
+          paddingTop: 15,
+          paddingBottom: 60,
+          marginTop: StatusBar.currentHeight,
+        }}
+      >
+        <HeaderApps />
+        <SearchBar />
       </View>
-      <Categories />
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <Categories />
+        <RestaurantItem />
+      </ScrollView>
     </SafeAreaView>
   );
 }
