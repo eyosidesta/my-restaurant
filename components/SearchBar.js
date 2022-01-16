996c185e3f7f2213e24a1949bfb1f666771f32d3
Eyosias Desta <37697169+eyosidesta@users.noreply.github.com>
2022-01-16 17:08:01 +0300

    Update SearchBar.js

--- components/SearchBar.js
+++ components/SearchBar.js
@@ -49,52 +49,6 @@ export default function SearchBar({cityHandler}) {
           search
         </Text>
       </View>
-      {/* <GooglePlacesAutocomplete
-      query={{key: "AIzaSyB_HE1zcZjhN6htuVbbFCU0kTqg25goRDw"}}
-        placeholder="Search"
-        onPress={(data, details = null) => {
-          console.log(data);
-          console.log(details);
-        }}
-
-        styles={{
-          textInput: {
-            fontWeight: "bold",
-            backgroundColor: "#eee",
-            borderRadius: 50,
-            marginTop: 7,
-            marginLeft: 10,
-            marginRight: 10
-          },
-          predefinedPlacesDescription: {
-            color: "#1faadb",
-          },
-        }}
-        // renderLeftButton={() => (
-        //   <View style={{ marginLeft: 10 }}>
-        //     <Ionicons name="location-sharp" size={24} />
-        //   </View>
-        // )}
-        // renderRightButton={() => (
-        //   <View
-        //     style={{
-        //       flexDirection: "row",
-        //       marginRight: 8,
-        //       backgroundColor: "white",
-        //       padding: 9,
-        //       borderRadius: 30,
-        //       alignItems: "center"
-        //     }}
-        //   >
-        //     <AntDesign
-        //       name="clockcircle"
-        //       size={11}
-        //       style={{ marginRight: 6 }}
-        //     />
-        //     <Text>Search</Text>
-        //   </View>
-        // )}
-      /> */}
     </View>
   );
 }
